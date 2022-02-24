b256fee1182a10d962a0bda99981262592442c46
dean <margintop3498@icloud.com>
2022-02-24 17:37:21 +0900

    latency 측정 버그 수정

--- README.md
+++ README.md
@@ -1,4 +1,63 @@
-# klaytn-stresstest (WIP)
+# klaytn-stresstest
 > 🚨 주의: 실제 운영중인 서버에 사용하시면 안됩니다.
 
 오토스케일링이 진행중일때 Liveness failure 를 일으키는지 테스트 합니다.
+
+```
+Total 55600
+Average Latency: 6.16ms
+Latest Latency: 5ms
+Healthy 55600
+Unhealthy 0
+BlockNumbers [84122421, 84122421, 84122421, 84122421, 84122421, 84122421, 84122421, 84122421, 84122421, 84122421]
+
+Total 55700
+Average Latency: 6.16ms
+Latest Latency: 5ms
+Healthy 55700
+Unhealthy 0
+BlockNumbers [84122421, 84122421, 84122421, 84122421, 84122421, 84122421, 84122421, 84122421, 84122421, 84122421]
+
+Total 55800
+Average Latency: 6.16ms
+Latest Latency: 6ms
+Healthy 55800
+Unhealthy 0
+BlockNumbers [84122421, 84122421, 84122421, 84122421, 84122421, 84122421, 84122421, 84122421, 84122421, 84122421]
+
+Total 55900
+Average Latency: 6.15ms
+Latest Latency: 4ms
+Healthy 55900
+Unhealthy 0
+BlockNumbers [84122421, 84122421, 84122421, 84122421, 84122421, 84122421, 84122421, 84122421, 84122421, 84122421]
+
+Total 56000
+Average Latency: 6.15ms
+Latest Latency: 7ms
+Healthy 56000
+Unhealthy 0
+BlockNumbers [84122421, 84122421, 84122421, 84122421, 84122421, 84122421, 84122421, 84122421, 84122421, 84122421]
+
+Total 56100
+Average Latency: 6.15ms
+Latest Latency: 4ms
+Healthy 56100
+Unhealthy 0
+BlockNumbers [84122421, 84122421, 84122421, 84122421, 84122421, 84122421, 84122421, 84122421, 84122421, 84122421]
+
+Total 56200
+Average Latency: 6.15ms
+Latest Latency: 4ms
+Healthy 56200
+Unhealthy 0
+BlockNumbers [84122421, 84122421, 84122421, 84122421, 84122421, 84122421, 84122421, 84122421, 84122421, 84122421]
+
+Total 56300
+Average Latency: 6.15ms
+Latest Latency: 5ms
+Healthy 56300
+Unhealthy 0
+BlockNumbers [84122421, 84122421, 84122421, 84122421, 84122421, 84122421, 84122421, 84122421, 84122421, 84122421]
+
+```
--- src/main.rs
+++ src/main.rs
@@ -4,44 +4,122 @@ use crate::caver::{
   types::{Block,BlockNumber},
   api::{Klay}
 };
+use std::sync::Arc;
+use tokio::time::sleep;
+use tokio::time::Instant;
+use ethereum_types::U64;
 
 mod caver;
 
 const RPC_URL: &str = "http://localhost:8551";
-const CONCURRENCY: i32 = 10;
-const BATCH_SIZE: i32 = 100;
 const DELAY: u64 = 0;
+const CONCURRENCY: i32 = 30;
+const BATCH_SIZE: i32 = 0;
 
-async fn too_many_blocks(thread_id: i32) -> caver::Result<()> {
-  let provider = caver::providers::Http::new(RPC_URL);
-  let caver = caver::Client::new(provider);
-  let klay = caver.klay();
+struct State {
+  pub total: i32,
+  pub healthy: i32,
+  pub unhealthy: i32,
+  pub total_latency: u128,
+  pub latest_latency: u128,
+  pub block_numbers: Vec<U64>
+}
 
-  if BATCH_SIZE == 0 {
-    loop {
-      println!("{}", thread_id);
-      klay.get_block_by_number(BlockNumber::Latest).await;
-      std::thread::sleep(std::time::Duration::from_millis(DELAY));
-    }
-  } else {
-    for j in 0..BATCH_SIZE {
-      println!("{}, {}", thread_id, j);
-      klay.get_block_by_number(BlockNumber::Latest).await;
-      std::thread::sleep(std::time::Duration::from_millis(DELAY));
+impl State {
+  pub fn new() -> Self {
+    Self{
+      total: 0,
+      healthy: 0,
+      unhealthy: 0,
+      total_latency: 0,
+      latest_latency: 0,
+      block_numbers: Vec::new()
     }
   }
 
-  Ok(())
+  pub fn success(&mut self, block_number: U64, latency: u128) {
+    self.total += 1;
+    self.healthy += 1;
+    self.total_latency += latency;
+    self.latest_latency = latency;
+    self.block_numbers.push(block_number);
+
+    if self.block_numbers.len() > 10 {
+      self.block_numbers.remove(0);
+    }
+
+    self.check_print_iteration();
+  }
+
+  pub fn failed(&mut self, latency: u128) {
+    self.total += 1;
+    self.unhealthy += 1;
+    self.total_latency += latency;
+    self.latest_latency = latency;
+
+    self.check_print_iteration();
+  }
+
+  pub fn check_print_iteration(&self) {
+    if self.total % 100 == 0 {
+      self.print();
+    }
+  }
+
+  pub fn print(&self) {
+    println!("Total {}", self.total);
+    println!("Average Latency: {:.2}ms", self.total_latency as f64 / self.total as f64);
+    println!("Latest Latency: {:.2}ms", self.latest_latency);
+    println!("Healthy {}", self.healthy);
+    println!("Unhealthy {}", self.unhealthy);
+    println!("BlockNumbers {:?}", self.block_numbers);
+    println!();
+  }
 }
 
 #[tokio::main]
 pub async fn main() -> caver::Result<()> {
   let mut handles = Vec::new();
+  let state = Arc::new(std::sync::Mutex::new(State::new()));
 
-  for i in 0..CONCURRENCY {
-    handles.push(tokio::spawn(async move { too_many_blocks(i).await }));
+  for _ in 0..CONCURRENCY {
+    let cloned_state = state.clone();
+
+    handles.push(tokio::spawn(async move {
+      let provider = caver::providers::Http::new(RPC_URL);
+      let caver = caver::Client::new(provider);
+      let klay = caver.klay();
+      let mut count = 0;
+
+      loop {
+        let now = Instant::now();
+
+        match klay.get_block_by_number(BlockNumber::Latest).await {
+          Ok(it) => {
+            match it {
+              Some(block) => {
+                cloned_state.lock().unwrap().success(block.number, now.elapsed().as_millis());
+              },
+              None => {}
+            }
+          }
+          Err(e) => {
+            cloned_state.lock().unwrap().failed(now.elapsed().as_millis())
+          }
+        }
+
+        if DELAY != 0 {
+          sleep(std::time::Duration::from_millis(DELAY)).await;
+        }
+
+        count += 1;
+
+        if BATCH_SIZE != 0 && count >= BATCH_SIZE {
+          break;
+        }
+      }
+    }));
   }
-
 
   for handle in handles {
     tokio::join!(handle);
